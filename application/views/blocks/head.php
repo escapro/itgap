69010bd78ef4d659c4d76762e67d81c5bf7d1d67
ruslan4742 <36818965+ruslan4742@users.noreply.github.com>
2019-10-12 20:46:23 +0400

    Доб. Вебмастера поисковых систем

--- application/views/blocks/head.php
+++ application/views/blocks/head.php
@@ -2,8 +2,9 @@
 <html lang="ru">
 <head>
 	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+	<meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<!-- <meta name="google-site-verification" content="2I45M7PBDeVLgx1uQn51I4t4YmJORZ0IFB-mUSzfGSA" /> -->
 	<?php if(APP_ENV == 'production'): ?>
 		<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(55693036, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/55693036" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131792796-2"></script>
@@ -43,7 +44,6 @@
 		<meta property="og:image" content="<?=base_url();?>media/images/logo.png">
 		<meta property="og:image:secure_url" content="<?=base_url();?>media/images/logo.png">
 	<?php endif; ?>
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<link rel="shortcut icon" href="/favicon/favicon.ico">
 	<link rel="stylesheet" type="text/css" href="/media/css/style.min.css">
 	<link rel="stylesheet" type="text/css" href="/media/fonts/Iconfonts/css/fontello.css">
