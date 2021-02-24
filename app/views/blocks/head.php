de00ea70af2fd706dcb24e1fe927611b0fa029bc
ruslan4742 <36818965+ruslan4742@users.noreply.github.com>
2021-02-24 11:20:37 +0400

    pass

--- app/views/blocks/head.php
+++ app/views/blocks/head.php
@@ -6,11 +6,7 @@
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
 
 	<?php if(APP_ENV == 'production'): ?>
-		<script data-ad-client="ca-pub-2285612636721407" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 		<script type="text/javascript" > (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)}; m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)}) (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(55693036, "init", { clickmap:true, trackLinks:true, accurateTrackBounce:true, webvisor:true }); </script> <noscript><div><img src="https://mc.yandex.ru/watch/55693036" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
-		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-131792796-2"></script>
-		<script>window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'UA-131792796-2');</script>
-		<meta name="google-site-verification" content="2I45M7PBDeVLgx1uQn51I4t4YmJORZ0IFB-mUSzfGSA" />
 	<?php endif; ?>
 
 	<script type="text/javascript" src="https://vk.com/js/api/openapi.js?167"></script>
